c86d6b5b47efe9649590e14e1a1bbaae15635e56
daniel petcov <danielpetcov.work@gmail.com>
2026-04-11 17:51:53 +0300

    updated the logging method of the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,24 @@ import configuration from 'config/configuration';
 import { AuthModule } from '@thallesp/nestjs-better-auth';
 import { auth } from './auth';
 
+import { LoggerModule } from 'pino-nestjs';
+
 @Module({
   imports: [
+    AuthModule.forRoot({ auth }),
     ConfigModule.forRoot({
       load: [configuration],
       cache: true,
     }),
-    AuthModule.forRoot({ auth }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? { target: 'pino-pretty' }
+            : undefined,
+        level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+      },
+    }),
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from 'pino-nestjs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bodyParser: false,
+    bufferLogs: true,
   });
+  app.useLogger(app.get(Logger));
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
